b14b688c59f313b41533e8bacccf552b0bfb7a52
sAkAm0t0 <sasakaka139@gmail.com>
2025-08-28 16:29:16 +0900

    fix: vimtex compile engine
    change compile engine from pdf to pdfdvi

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,10 +2,14 @@ return {
   {
     "lervag/vimtex",
     init = function()
+      vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-pdfdvi" }
       vim.g.vimtex_compiler_method = "latexmk"
       vim.g.vimtex_compiler_latexmk = {
         executable = "run_platex",
-        options = { "-synctex=1", "-halt-on-error" },
+        options = {
+          "-synctex=1",
+          "-halt-on-error",
+        },
         continuous = 1,
       }
       vim.g.vimtex_view_method = "general"
